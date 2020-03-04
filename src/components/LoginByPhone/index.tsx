f5333307cd7fd680c6a37a36e9961efad568a55f
Ouchuxuan <ocx7793709@hotmail>
2020-03-04 23:53:24 +0800

    增加手机登录

--- src/components/LoginByPhone/index.tsx
+++ src/components/LoginByPhone/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { Link, RouteComponentProps } from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import styles from './LoginByPhone.module.scss';
-import { NavBar, Icon, InputItem } from 'antd-mobile';
+import { NavBar, Icon, InputItem, List } from 'antd-mobile';
 
 const LoginByPhone: React.FC<RouteComponentProps> = (props) => {
+  type phoneNum = string
+  const [phoneNum, setPhoneNum] = useState<phoneNum>('');
+  const submit = useCallback(
+    () => {
+      if (!(/^1[3456789]\d{9}$/.test(phoneNum))) {
+        alert("手机号码有误，请重填");
+      } else {
+        props.history.push('/')
+      }
+
+    },
+    [phoneNum,props.history],
+  )
   return (
     <div className={styles.loginByPhoneLayout}>
       <NavBar
@@ -11,9 +24,13 @@ const LoginByPhone: React.FC<RouteComponentProps> = (props) => {
         icon={<Icon type="left" />}
         onLeftClick={() => props.history.goBack()}
       >手机号登录</NavBar>
-      <div className="loginNote">未注册手机号登录后将自动创建账号</div>
-      <InputItem type="phone" placeholder="请输入手机号码" clear={true} />
-      <Link to='/login_by_phone' className={styles.linkButton}>下一步</Link>
+      <div className={styles.mainLayout}>
+        <div className={styles.loginNote}>未注册手机号登录后将自动创建账号</div>
+        <List>
+          +86<InputItem type="phone" value={phoneNum} className={styles.inputForm} placeholder="请输入手机号码" clear={true} onChange={val => { setPhoneNum(val) }} />
+        </List>
+        <div className={styles.nextStep} onClick={() => submit()}>下一步</div>
+      </div>
     </div>
   )
 }
